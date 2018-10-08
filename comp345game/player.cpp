3bd7e39d912255efbde5c7c1533ba71e5de79c6b
Lucas Kourouklis <lucas.kourouklis@gmail.com>
2018-10-08 17:44:06 -0400

    Added Card and MonsterCard related functions to Player

--- comp345game/player.cpp
+++ comp345game/player.cpp
@@ -4,36 +4,55 @@
 #include <ctime>
 #include <string>
 using namespace std;
-player::player()
-{
+player::player() {
 	cout << "I have been initialized " << endl;
 }
-void player::setRegion(string reg)
-{
+
+void player::setRegion(string reg) {
 	region = reg;
 }
-void player::setMonsterCard(string monst)
-{
-	monsterCard = monst;
-}
-string player::getRegion()
-{
+
+string player::getRegion() {
 	return region;
 }
-string player::getMonsterCard()
-{
+
+void player::setMonsterCard(MonsterCard monst) {
+	monsterCard = monst;
+}
+
+MonsterCard player::getMonsterCard() {
 	return monsterCard;
 }
-void player::rollDice()
-{
+
+bool player::addOwnedCard(GameCard card) {
+	std::string cardName = card.getName();
+	bool cardNotOwned = true;
+	for (GameCard c : ownedCards) {
+		if (c.getName() == cardName) {
+			cardNotOwned == false;
+		}
+	}
+
+	if (cardNotOwned)
+		ownedCards.push_back(card);
+	else
+		return false;
+
+	return true;
+}
+
+std::vector<GameCard> player::getOwnedCards() {
+	return ownedCards;
+}
+
+void player::rollDice() {
 
 }
-void player::resolveDice()
-{
 
-}
-void player::move()
-{
+void player::resolveDice() {
 
 }
 
+void player::move() {
+
+}
--- comp345game/player.h
+++ comp345game/player.h
@@ -1,23 +1,35 @@
 #pragma once
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "gameCard.h"
+#include "monsterCard.h"
+#include <vector>
 #include <string>
+
 using namespace std;
-class player
-{
-public:
-	player();
-	void setRegion(string reg);
-	void setMonsterCard(string monst);
-	string getRegion();
-	string getMonsterCard();
-	void rollDice();
-	void resolveDice();
-	void move();
-
-	
-
+class player {
 private:
-	string monsterCard;
+	MonsterCard monsterCard;
+	std::vector<GameCard> ownedCards;
 	string region;
 	//dice* diceContainer[6];
 
+public:
+	player();
+	void setRegion(string reg);
+	string getRegion();
+
+	void setMonsterCard(MonsterCard monst);
+	MonsterCard getMonsterCard();
+
+	bool addOwnedCard(GameCard monst);
+	std::vector<GameCard> getOwnedCards();
+
+	void rollDice();
+	void resolveDice();
+
+	void move();
 };
+
+#endif
